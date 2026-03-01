fed6786c113208ff2ab463c2776323b1dc6e4b64
ayaauuu05 <a_abay@kbtu.kz>
2026-03-01 22:52:41 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,28 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x = "Python is awesome"
+print(x)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
